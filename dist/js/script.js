c64100248315a1499286ef2717173a6b3330f094
Jahir Hasan <hasanjahir14@gmail.com>
2021-02-14 19:08:40 +0600

    done product-detail, news, news-single page

--- dist/js/script.js
+++ dist/js/script.js
@@ -107,3 +107,39 @@ $(function() {
     });
     
 });
+
+$('.related-product-slider').slick({
+    infinite: true,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 5000,
+    responsive: [
+        {
+            breakpoint: 1200,
+            settings: {
+                slidesToShow: 4,
+            }
+        },
+        {
+            breakpoint: 1024,
+            settings: {
+                slidesToShow: 3,
+            }
+        },
+        {
+            breakpoint: 600,
+            settings: {
+                slidesToShow: 2
+            }
+        },
+        {
+            breakpoint: 480,
+            settings: {
+                slidesToShow: 1
+            }
+        }
+    ],    
+    prevArrow: "<button class='slick-prev'><i class='fas fa-angle-left'></i></button>",
+    nextArrow: "<button class='slick-next'><i class='fas fa-angle-right'></i></button>"
+});
